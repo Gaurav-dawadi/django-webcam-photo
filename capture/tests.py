f69c90c53445239d0e95d12bf42f44e63615a6b2
Gaurav-dawadi <gaurabdawadi@gamil.com>
2021-01-19 18:40:04 +0545

    test: write testcase for url

--- capture/tests.py
+++ capture/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse, resolve 
 
 # Create your tests here.
+
+client = Client()
+
+class TestURL(TestCase):
+    def test_home_url(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_webcam_url(self):
+        response = self.client.get(reverse('webcam'))
+        self.assertEqual(response.status_code, 200)
